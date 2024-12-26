7fb7f4df9870a82122f7a1307848ba77f0a28f17
Aleš Vaupotič <ales@vaupotic.eu>
2024-12-26 18:50:30 +0100

    cleanup

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,19 +1,11 @@
 import { sequence } from '@sveltejs/kit/hooks';
 import { PUBLIC_DEFAULT_LOCALE } from '$env/static/public';
-import { languages, translations } from '$i18n/translations';
-import { redirect } from '@sveltejs/kit';
+import { translations } from '$i18n/translations';
 
 export async function handle_language({ event, resolve }) {
 	let language = event.params.language || PUBLIC_DEFAULT_LOCALE;
 	event.locals.language = language; // make it available to page.data
 	event.locals.translations = translations[language];
-
-	if (languages.includes(language) && !event.params.slug) {
-		// we have a language but no slug, this is language home page
-		redirect(303, '/' + language + '/');
-	}
-
-	console.log('event.locals:', event.locals.language);
 
 	return await resolve(event, { transformPageChunk: ({ html }) => html.replace('%html-lang%', language) });
 }
